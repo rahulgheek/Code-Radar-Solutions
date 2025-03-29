053b90146523199260e5a57a49bae9aefab64337
Rahul Gheek <rahulgheek1@gmail.com>
2025-03-29 13:32:10 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -3,4 +3,13 @@
 
 void caesarCipher(char str[],int shift,char message[]){
     strcpy(message,str);
+    int i = 0;
+    while(message[i] != '\0'){
+        if(message[i] == ' '){
+            i++;
+            continue;
+}
+        message[i] += shift;
+        i++;
+    }
 }
